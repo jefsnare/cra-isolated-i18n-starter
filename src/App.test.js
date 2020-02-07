39c781b5c1c2621b7c20b0d86ccac02efa70faa9
Jeffrey de Graaf <jeffrey.de.graaf@avivasolutions.nl>
2020-02-07 11:59:03 +0100

    fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Catalog/i);
+  const linkElement = getByText(/index/i);
   expect(linkElement).toBeInTheDocument();
 });
